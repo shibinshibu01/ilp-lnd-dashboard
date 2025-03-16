00307ef26899e2855e393cd862d63a722b10ce39
Aswin A <aswin01010@gmail.com>
2025-03-16 22:08:27 +0530

    feat: modal can now be opened by clicking the course card

--- src/services/script.js
+++ src/services/script.js
@@ -47,7 +47,7 @@ axios.get(url)
         const [technicalCourses, softSkillsCourses, behavioralCourses] = courseCategoriesFunction();
 
         const courseCategoriesChart = document.getElementById('courseCategories').getContext('2d');
-        
+
         const myChart = new Chart(courseCategoriesChart, {
             type: 'bar',
             data: {
@@ -66,12 +66,12 @@ axios.get(url)
                 },
                 plugins: {
                     legend: {
-                        display: false 
+                        display: false
                     }
                 }
             }
         });
-        
+
     })
     .catch(error => {
         console.error('Axios Error:', error);
@@ -109,10 +109,11 @@ function getDaySuffix(day) {
 
 function populateModalData(courseId) {
     const course = data.courses[courseId];
+    console.log(courseId);
 
     document.getElementById('courseImage').src = course.courseBannerImage;
 
-    const typeElement = document.getElementById('courseType');
+    const typeElement = document.getElementById('modalCourseType');
     typeElement.textContent = course.courseType;
     typeElement.className = 'course-modal__tag';
     if (course.courseType === 'Technical') {
@@ -125,10 +126,12 @@ function populateModalData(courseId) {
 
     document.getElementById('courseName').textContent = course.courseName;
     document.getElementById('courseDescription').textContent = course.shortDescription;
-    document.getElementById('courseDuration').textContent = `${course.duration} hrs`;
-    document.getElementById('courseStartDate').textContent = formatDate(course.startDate);
+    document.getElementById('modalCourseDuration').textContent = `${course.duration} hrs`;
+    document.getElementById('modalCourseStartDate').textContent = formatDate(course.startDate);
+    console.log(course.duration);
 
-    const statusElement = document.getElementById('courseStatus');
+
+    const statusElement = document.getElementById('modalCourseStatus');
     statusElement.textContent = course.status;
     statusElement.className = 'course-modal__detail-value';
     if (course.status === 'completed') {
@@ -147,7 +150,7 @@ function populateModalData(courseId) {
     /////////////////////////////////
     document.getElementById('courseTrainer').textContent = course.trainerName;
     document.getElementById('courseTypeDetails').textContent = course.courseType.toLowerCase();
-    document.getElementById('courseMode').textContent = course.mode.toLowerCase();
+    document.getElementById('modalCourseMode').textContent = course.mode.toLowerCase();
 
     document.getElementById('courseFeedback').textContent = course.feedbackScore.toFixed(1);
     document.getElementById('courseEffectiveness').textContent = course.effectiveness.toFixed(1);
@@ -173,106 +176,121 @@ courseModalBackdrop.addEventListener("click", function (event) {
 });
 //to display courses
 axios.get(url)
-        .then(response => {
-            console.log('Axios Data:', response.data);
-            const courses=response.data.courses;
-            
-            const courseContainer = document.querySelector(".course-container");
-            let newContent = "";
-            
-            Object.keys(courses).forEach(key=>{
-                const course=courses[key];
-                let courseTypeClass = "";
-                if (course.courseType === "Softskills") {
-                        courseTypeClass = "softskills-course";
-                } else if (course.courseType === "Technical") {
-                        courseTypeClass = "technical-course";
-                } else if (course.courseType === "Behavioural") {
-                        courseTypeClass = "behavioral-course";
-                }
-                let coursestatusClass = "";
-                if (course.status === "completed") {
-                        coursestatusClass = "completed-class";
-                } else if (course.status === "in-progress") {
-                        coursestatusClass = "inprogress-class";
-                } else if (course.status === "scheduled") {
-                        coursestatusClass = "scheduled-class";
-                }
-                newContent += `
-                    <div class="course-card">
-                        <div class="course-type-badge">
-                            <button id="courseType" class="${courseTypeClass}">${course.courseType}</button>
-                        </div>
-                        <img class="course-img" src="${course.courseBannerImage}">
-                        <div class="course-content">
-                            <div class="title-type">
-                                <h3 id="courseTitle">${course.courseName}</h3>
-                            </div>
-                            <p id="courseDesc">${course.shortDescription}</p>
-                            <p id="courseDuration">duration: <span class="dynamic-content">${course.duration} hrs</span><p>
-                            <p id="courseStartDate">start date: <span class="dynamic-content">${formatDate(course.startDate)}</span></p>
-                            <div class="coursestatus-mode">
-                                <p id="courseStatus">status: <span class="${coursestatusClass}">${course.status}</span></p>
-                                <p id="courseMode"><span class="dynamic-content_mode">${course.mode}</span></p>
-                            </div>
-                        </div>
-                    </div>`;
+    .then(response => {
+        console.log('Axios Data:', response.data);
+        const courses = response.data.courses;
+
+        const courseContainer = document.querySelector(".course-container");
+        let newContent = "";
+
+        Object.keys(courses).forEach(key => {
+            const course = courses[key];
+            let courseTypeClass = "";
+            if (course.courseType === "Softskills") {
+                courseTypeClass = "softskills-course";
+            } else if (course.courseType === "Technical") {
+                courseTypeClass = "technical-course";
+            } else if (course.courseType === "Behavioural") {
+                courseTypeClass = "behavioral-course";
+            }
+            let coursestatusClass = "";
+            if (course.status === "completed") {
+                coursestatusClass = "completed-class";
+            } else if (course.status === "in-progress") {
+                coursestatusClass = "inprogress-class";
+            } else if (course.status === "scheduled") {
+                coursestatusClass = "scheduled-class";
+            }
+
+            newContent += `
+    <div class="course-card" data-course-id="${key}"> 
+        <div class="course-type-badge">
+            <button id="courseType" class="${courseTypeClass}">${course.courseType}</button>
+        </div>
+        <img class="course-img" src="${course.courseBannerImage}">
+        <div class="course-content">
+            <div class="title-type">
+                <h3 id="courseTitle">${course.courseName}</h3>
+            </div>
+            <p id="courseDesc">${course.shortDescription}</p>
+            <p id="courseDuration">duration: <span class="dynamic-content">${course.duration} hrs</span><p>
+            <p id="courseStartDate">start date: <span class="dynamic-content">${formatDate(course.startDate)}</span></p>
+            <div class="coursestatus-mode">
+                <p id="courseStatus">status: <span class="${coursestatusClass}">${course.status}</span></p>
+                <p id="courseMode"><span class="dynamic-content_mode">${course.mode}</span></p>
+            </div>
+        </div>
+    </div>`;
+
+
+
+
+
+        });
+
+        courseContainer.innerHTML = newContent;
+
+        document.querySelectorAll('.course-card').forEach(card => {
+            card.addEventListener("click", function () {
+                console.log("+" + card.dataset.key);
+                openCourseModal(card.dataset.courseId);
+
+
             });
+        });
 
-            courseContainer.innerHTML = newContent;
+        const prevBtn = document.getElementById("prevBtn");
+        const nextBtn = document.getElementById("nextBtn");
 
-            const prevBtn = document.getElementById("prevBtn");
-            const nextBtn = document.getElementById("nextBtn");
+        function formatDate(dateString) {
+            const date = new Date(dateString);
+            const day = date.getDate();
+            const month = date.toLocaleString('en-US', { month: 'short' });
+            const year = date.getFullYear();
 
-            function formatDate(dateString) {
-                const date = new Date(dateString);
-                const day = date.getDate(); 
-                const month = date.toLocaleString('en-US', { month: 'short' }); 
-                const year = date.getFullYear();
-            
-                return `${day} ${month} ${year}`;
-            }
+            return `${day} ${month} ${year}`;
+        }
 
-            let autoScroll;
+        let autoScroll;
 
-            function startAutoScroll(){
-                stopAutoScroll();
-                autoScroll=setInterval(() => {
-                    courseContainer.scrollBy({left:320,behavior:'smooth'});
+        function startAutoScroll() {
+            stopAutoScroll();
+            autoScroll = setInterval(() => {
+                courseContainer.scrollBy({ left: 320, behavior: 'smooth' });
 
-                    if (courseContainer.scrollLeft + courseContainer.clientWidth >= courseContainer.scrollWidth) {
-                        setTimeout(() => {
-                            courseContainer.scrollTo({ left: 0, behavior: 'smooth' });
-                        }, 500); 
-                    }
-                }, 2500);
-            }
+                if (courseContainer.scrollLeft + courseContainer.clientWidth >= courseContainer.scrollWidth) {
+                    setTimeout(() => {
+                        courseContainer.scrollTo({ left: 0, behavior: 'smooth' });
+                    }, 500);
+                }
+            }, 2500);
+        }
 
-            function stopAutoScroll(){
-                clearInterval(autoScroll);
-            }
+        function stopAutoScroll() {
+            clearInterval(autoScroll);
+        }
 
-            nextBtn.addEventListener("click",()=>{
-                stopAutoScroll();
-                courseContainer.scrollBy({left:320,behavior:'smooth'});
-                setTimeout(startAutoScroll(),5000);
-            })
-            
-            prevBtn.addEventListener("click",()=>{
-                stopAutoScroll();
-                courseContainer.scrollBy({left:-320,behavior:'smooth'});
-                setTimeout(startAutoScroll(),5000);
-            })
-
-            courseContainer.addEventListener("mouseenter", stopAutoScroll());
-            courseContainer.addEventListener("mouseleave", startAutoScroll());
-
-            startAutoScroll();
+        nextBtn.addEventListener("click", () => {
+            stopAutoScroll();
+            courseContainer.scrollBy({ left: 320, behavior: 'smooth' });
+            setTimeout(startAutoScroll(), 5000);
         })
-        .catch(error=>{
-            console.error('Axios Error: ',error);
-            document.querySelector(".course-container").innerHTML = "<p>Error fetching courses.</p>";
+
+        prevBtn.addEventListener("click", () => {
+            stopAutoScroll();
+            courseContainer.scrollBy({ left: -320, behavior: 'smooth' });
+            setTimeout(startAutoScroll(), 5000);
         })
+
+        courseContainer.addEventListener("mouseenter", stopAutoScroll());
+        courseContainer.addEventListener("mouseleave", startAutoScroll());
+
+        startAutoScroll();
+    })
+    .catch(error => {
+        console.error('Axios Error: ', error);
+        document.querySelector(".course-container").innerHTML = "<p>Error fetching courses.</p>";
+    })
 
 
 
