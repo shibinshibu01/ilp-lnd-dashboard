f35ffee77e81db89ad9af65cec06fe9f7ff11ab3
Mariyam Emamudeen <mariyam.emamudeen@experionglobal.local>
2025-03-31 21:16:22 +0530

    feature:basic css and scroll

--- src/services/script.js
+++ src/services/script.js
@@ -22,21 +22,42 @@ function displayTrainings(trainings) {
     trainingList.innerHTML = ""; // Clear previous content
 
     Object.keys(trainings).forEach(trainingId => {
-        const training = trainings[trainingId]; // Get training object
+        const training = trainings[trainingId];
+        
+        let typeClass = "";
+        if (training.training_type === "softskills") {
+            typeClass = "soft-skills";
+        } else if (training.training_type === "language") {
+            typeClass = "language";
+        } else if (training.training_type === "technical") {
+            typeClass = "technical";
+        }
+       
+        let statusClass = "";
+        if (training.status === "completed") {
+            statusClass = "completed";
+        } else if (training.status === "in-progress") {
+            statusClass = "in-progress";
+        } else if (training.status === "scheduled") {
+            statusClass = "scheduled";
+        }
         let listItem = `
-        <div class="training-item">
-            <h3>${training.training_name}</h3>
-            <p id="type">${training.training_type}</p>
-            <h2>${training.duration}</h2>
-            <p>${training.target_audience}</p>
-            <p>${training.trainer}</p>
+        <div class="training-item" >
+            <p id="training__title" onclick="loadTrainingDetails('${trainingId}')">${training.training_name}</p>
+            <p id="type" class="${typeClass}">${training.training_type}</p>
+            <p id="duration">${training.duration}hrs</p>
+            <p id="target_audience">${training.target_audience}</p>
+            <p id="trainer">${training.trainer}</p>
             <p id="metrics">${training.employees_attended}</p>
             <p id="metrics">${training.attendance}%</p>
             <p id="metrics">${training.effectiveness_score}</p>
-            <p>${training.mode}</p>
-            <p>${training.status}</p>
+            <p id="mode">${training.mode}</p>
+            <p id="status" class="${statusClass}">${training.status}</p>
         </div>`;
 
         trainingList.insertAdjacentHTML("beforeend", listItem); // Append without overwriting
     });
 }
+
+
+
